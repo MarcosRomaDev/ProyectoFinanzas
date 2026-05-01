d78d0f3506e6d64d86d3cf6ad1c039857880f428
MarcosRomaDev <marcos.roma.dev@gmail.com>
2026-05-01 10:38:31 +0200

    Creado arbol de carpetas y añadidas dependencias de SpringInitializr para backend

---
+++ finanzas-backend/finanzas-backend/src/test/java/com/marcos/finanzas_backend/FinanzasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.finanzas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
