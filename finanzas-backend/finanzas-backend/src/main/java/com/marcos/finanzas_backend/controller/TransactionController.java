cec849ba525597f50fc3bf138dea828450d54132
MarcosRomaDev <marcos.roma.dev@gmail.com>
2026-05-01 18:51:46 +0200

    Añadidos inserts de prueba a schema.sql. Implementados CategoryController y TransactionController, con las consultas del MVP.

---
+++ finanzas-backend/finanzas-backend/src/main/java/com/marcos/finanzas_backend/controller/TransactionController.java
@@ -0,0 +1,33 @@
+package com.marcos.finanzas_backend.controller;
+
+import com.marcos.finanzas_backend.entity.Transaction;
+import com.marcos.finanzas_backend.repository.TransactionRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+    private final TransactionRepository repository;
+
+    public TransactionController(TransactionRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping("path")
+    public List<Transaction> findAlList() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Transaction saveTransaction(@RequestBody Transaction nuevaTransaction) {
+        return repository.save(nuevaTransaction);
+    }
+}
