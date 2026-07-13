6902e44831e6bba3117a39f40c76abf09ff2e221
MarcosRomaDev <marcos.roma.dev@gmail.com>
2026-07-13 23:11:58 +0200

    Apuntar el frontend al backend desplegado en Render

--- finanzas-frontend/js/services/category.service.js
+++ finanzas-frontend/js/services/category.service.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/api/categories";
+const API_URL = "https://proyectofinanzas-x5wl.onrender.com/api/categories";
 
 export const CategoryService = {
   async fetchCategories() {
